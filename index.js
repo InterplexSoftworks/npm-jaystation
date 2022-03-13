1ab398ee86a16c6eab5473a0a22ba3c31174da70
dalk <admin@thecool.link>
2022-03-12 20:26:13 -0600

    f

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const randomNumberGenerator = require('./penis.js'); //assuming its on the same folder as index.js
-const ratebig = randomNumberGenerator(1,99)
-const ratesmall = randomNumberGenerator(1,9)
 
 exports.GayPercent = function() {
+    const ratebig = randomNumberGenerator(1,99)
+const ratesmall = randomNumberGenerator(1,9)
     const GayRate =  `JayStation is ${ratebig}.${ratesmall}% gay.`
         return GayRate
     }
